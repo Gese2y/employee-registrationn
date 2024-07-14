4effeebc74d20ddf5543e655e91d21a05c8eb66a
gese <geseseteshome21@gmail.com>
2024-07-14 15:17:23 -0700

    second commit

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { registerLicense } from '@syncfusion/ej2-base';
 import { AppModule } from './app/app.module';
 
-
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NCaF5cXmZCeUx0RXxbf1x0ZFdMYVRbQHNPIiBoS35RckVkWHxfdHVdQmVVUEF1');
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
